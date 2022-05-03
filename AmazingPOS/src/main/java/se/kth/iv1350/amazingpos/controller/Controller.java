dc3dcc435a4216e602b6ed00bef66853b152dbbb
KCchronos <sushilkc855@outlook.com>
2022-05-03 11:55:53 +0200

    Add files via upload

---
+++ AmazingPOS/src/main/java/se/kth/iv1350/amazingpos/controller/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package se.kth.iv1350.amazingpos.controller;
+
+/**
+ * This is the application only controller. All calls to the model pass through this class. 
+ * @author sushi
+ */
+public class Controller {
+    
+}
